d14a508299fdc01059efb8159899856dcd7c53a0
Roman Gontar <ashenoooone@gmail.com>
2022-07-08 23:53:56 +0400

    CLIENT:feat:login thunk ready

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { loginUser } from '../../store/slices/thunks/userLoginThunk';
 import './Login.scss';
 const Login = () => {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const onEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onPasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(loginUser({ email, password }));
+  };
+
   return (
     <section className='login'>
       <div className='login__container'>
         <h1 className='login__title'>Войти</h1>
-        <form className='login__form form'>
+        <form className='login__form form' onSubmit={onSubmit}>
           <input
             type='email'
             className='login__input input'
             placeholder='Почта'
-          />
-          <input
-            type='text'
-            className='login__input input'
-            placeholder='Имя пользователя'
+            onChange={onEmailChange}
+            value={email}
+            required
           />
           <input
             type='password'
             className='login__input input'
             placeholder='Пароль'
+            onChange={onPasswordChange}
+            value={password}
+            required
           />
           <button type='submit' className='button button_default'>
             Войти
--- client/src/store/slices/thunks/userLoginThunk.js
+++ client/src/store/slices/thunks/userLoginThunk.js
@@ -6,12 +6,13 @@ export const loginUser = createAsyncThunk(
   async function ({ email, password }, { rejectWithValue }) {
     try {
       const responce = await axios.post(
-        'http://localhost:5000/api/user/registration',
+        'http://localhost:5000/api/user/login',
         {
           email: email,
           password: password,
         }
       );
+      console.log(responce.data);
       localStorage.setItem('jwt', responce.data.token);
       return responce.data;
     } catch (error) {
